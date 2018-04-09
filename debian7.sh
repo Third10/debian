8ea1d67637c6d5a4aa6acfce49607950c07ec6a4
thirdza056 <33078689+thirdza056@users.noreply.github.com>
2018-04-09 19:36:30 +0700

    Update debian7.sh

--- debian7.sh
+++ debian7.sh
@@ -100,24 +100,24 @@ service nginx restart
 
 # install openvpn
 cd
-# apt-get -y install openvpn
-# wget -q -O /etc/openvpn/openvpn.tar "https://raw.githubusercontent.com/Third10/debian/master/app/openvpn-debian.tar"
-# cd /etc/openvpn/
-# tar xf openvpn.tar
-# wget -q -O /etc/openvpn/1194.conf https://raw.githubusercontent.com/Third10/debian/master/null/1194.conf
-# service openvpn restart
-# sysctl -w net.ipv4.ip_forward=1
-# sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
-# wget -q -O /etc/iptables.up.rules https://raw.githubusercontent.com/Third10/debian/master/null/iptables.up.rules
-# sed -i '$ i\iptables-restore < /etc/iptables.up.rules' /etc/rc.local
-# sed -i $MYIP2 /etc/iptables.up.rules;
-# iptables-restore < /etc/iptables.up.rules
-# service openvpn restart
+apt-get -y install openvpn
+wget -q -O /etc/openvpn/openvpn.tar "https://raw.githubusercontent.com/Third10/debian/master/app/openvpn-debian.tar"
+cd /etc/openvpn/
+tar xf openvpn.tar
+wget -q -O /etc/openvpn/1194.conf https://raw.githubusercontent.com/Third10/debian/master/null/1194.conf
+service openvpn restart
+sysctl -w net.ipv4.ip_forward=1
+sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
+wget -q -O /etc/iptables.up.rules https://raw.githubusercontent.com/Third10/debian/master/null/iptables.up.rules
+sed -i '$ i\iptables-restore < /etc/iptables.up.rules' /etc/rc.local
+sed -i $MYIP2 /etc/iptables.up.rules;
+iptables-restore < /etc/iptables.up.rules
+service openvpn restart
 
 #configure openvpn client config
-# cd /etc/openvpn/
-# wget -q -O /etc/openvpn/1194-client.ovpn https://raw.githubusercontent.com/Third10/debian/master/null/1194-client.conf
-# sed -i $MYIP2 /etc/openvpn/1194-client.ovpn;
+cd /etc/openvpn/
+wget -q -O /etc/openvpn/1194-client.ovpn https://raw.githubusercontent.com/Third10/debian/master/null/1194-client.conf
+sed -i $MYIP2 /etc/openvpn/1194-client.ovpn;
 PASS=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 15 | head -n 1`;
 useradd -M -s /bin/false admin_ibnu
 echo "admin_ibnu:$PASS" | chpasswd
